b47ca6391ea5f550f479fc9270a593a6ce7030bc
Dan Nicodemo <dan@showcaseidx.com>
2016-12-01 12:53:19 -0500

    added the ability to quit by typing in exit or quit as the name, added a clear screen for ease of use

--- compiler.rb
+++ compiler.rb
@@ -6,24 +6,36 @@ def compiler
 	puts "Welcome to the hotsheet code compiler!"
 	puts "Please enter the building or development name:"
 	name = gets.chomp
-	puts "#{name}"
-	puts "Please enter the building address:"
-	address = gets.chomp
-	puts "Please enter the city in question:"
-	city = gets.chomp
-	puts "#{name} - #{address} - #{city}"
-	puts "Do you need to add anymore information?"
-	answer = gets.chomp
-	answer = answer.downcase
-	if answer == "yes"
-		puts "Please choose which info you need to add:"
-		info = gets.chomp
+	if name.downcase == "quit" || name.downcase == "exit"
+		abort("Thank you for using compiler!")
 	else
-		puts "\[showcaseidx_custom name = \"#{name}\" restrict=\'address = \"#{address}\"\'\] "
-		puts "EQ_city=#{city}, FL"
-		puts "EQ_num_bedrooms=0"
-		puts "EQ_num_full_bathrooms=0"
-		puts "\[/showcaseidx_custom\]"
+		puts "Please enter the building address:"
+		address = gets.chomp
+		puts "Please enter the city in question:"
+		city = gets.chomp
+		puts "Do you need to change anything?"
+		puts " "
+		puts "#{name} - #{address} - #{city}"
+		puts " "
+		print ">"
+		answer = gets.chomp
+		answer = answer.downcase
+		if answer == "yes"
+			compiler
+		else
+			system('clear')
+			puts " "
+			puts " "
+			puts "\[showcaseidx_custom name = \"#{name}\" restrict=\'address = \"#{address}\"\'\] "
+			puts "EQ_city=#{city}, FL"
+			puts "EQ_num_bedrooms=0"
+			puts "EQ_num_full_bathrooms=0"
+			puts "\[/showcaseidx_custom\]"
+		end
+		puts " "
+		puts " "
+		puts " "
+		compiler
 	end
 end
 
